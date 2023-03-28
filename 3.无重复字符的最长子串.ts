42aded729513d0ef4ffddc2c6a5c01165add5165
hope <1962926166@qq.com>
2023-03-28 08:30:35 +0800

    hot 100

--- 3.无重复字符的最长子串.ts
+++ 3.无重复字符的最长子串.ts
@@ -6,7 +6,7 @@
 
 // @lc code=start
 // 滑动窗口
-function lengthOfLongestSubstring(s: string): number {
+/* function lengthOfLongestSubstring(s: string): number {
   let ret = 0;
   const n = s.length;
   // 记录每个字符是否出现过
@@ -25,6 +25,25 @@ function lengthOfLongestSubstring(s: string): number {
     }
     // 第l到r个字符是一个极长的无重复字符子串
     ret = Math.max(ret, r - l + 1);
+  }
+  return ret;
+} */
+
+// 两个瓶颈
+function lengthOfLongestSubstring(s: string): number {
+  let ret = 0;
+  const n = s.length;
+  // 记录每个字符上一次出现的索引
+  const map: number[] = new Array(256).fill(-1);
+  // 前一个字符的瓶颈
+  let pre = -1;
+  for (let i = 0; i < n; i++) {
+    const charCode = s[i].charCodeAt(0);
+    // map[charCode]为当前字符上一次出现的索引
+    // 在两者之间取最大值作为当前字符的瓶颈
+    pre = Math.max(pre, map[charCode]);
+    ret = Math.max(ret, i - pre);
+    map[charCode] = i;
   }
   return ret;
 }
